5ecf46ae2c2db8e837971063a7d87d8a9797624e
Artur Guimarães <arturguimaraes.sk8@gmail.com>
2023-08-27 20:20:54 -0300

    feat: added the route to request at "bored api" (routes.js)

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
     res.status(200).send('Essa é o app do grupo 5');
 });
 
-router.get('/cn-jokes', async (req, res) => {
+router.get('/api/piadas', async (req, res) => {
     axios.get('https://api.chucknorris.io/jokes/random')
         .then((response) => {
             const apiData = response.data;
@@ -22,12 +22,33 @@ router.get('/cn-jokes', async (req, res) => {
         });
 });
 
+router.get('/api/atividades', async (req, res) => {
+    axios.get('https://www.boredapi.com/api/activity')
+        .then((response) => {
+            const boredData = response.data;
+            res.json(boredData);
+    })
+});
+
 module.exports = router;
 
+// Retorno API CHUCK NORRIS
 /* {"categories":[],
 "created_at":"2020-01-05 13:42:24.142371",
 "icon_url":"https://assets.chucknorris.host/img/avatar/chuck-norris.png",
 "id":"YSHDZHoTSuqUWoDPdzioLw",
 "updated_at":"2020-01-05 13:42:24.142371",
 "url":"https://api.chucknorris.io/jokes/YSHDZHoTSuqUWoDPdzioLw",
-"value":"The arc of triumph in Paris is Chuck Norris cock-ring."} */
+"value":"The arc of triumph in Paris is Chuck Norris cock-ring."} 
+*/
+
+// RETORNO API BORED-API
+/* 
+{"activity":"Practice coding in your favorite lanaguage",
+"type":"recreational",
+"participants":1,
+"price":0,
+"link":"",
+"key":"7096020",
+"accessibility":0.1}
+*/
