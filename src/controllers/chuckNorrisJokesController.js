05f3665cb73adecb87992688d4bbf5d51d3cc16a
Artur Guimarães <arturguimaraes.sk8@gmail.com>
2023-08-31 10:15:56 -0300

    feat: added GUID generator on chuckNorrisJokescontroller.js

--- src/controllers/chuckNorrisJokesController.js
+++ src/controllers/chuckNorrisJokesController.js
@@ -2,16 +2,17 @@ const axios = require('axios');
 const { formatDate } = require('../utils/functions/formatDate.js');
 const { replaceChuckNorris } = require('../utils/functions/replaceChuckNorris.js');
 const { AppStrings } = require('../utils/constants/appStrings.js');
-
+const { guidGenerator } = require('../utils/functions/guidGenerator.js');
 
 async function ChuckNorrisJokesData() {
     const response = await axios.get(AppStrings.api_cn_url);
     const jokesResponse = response.data;
+    const newGuid = guidGenerator();
     let formatedJokesResponse = {
         data_atualizacao: formatDate(jokesResponse.updated_at),
         data_criacao: formatDate(jokesResponse.created_at),
         icone: jokesResponse.icon_url,
-        id: jokesResponse.id,
+        id: newGuid,
         piada: replaceChuckNorris(jokesResponse.value),
         referencia: jokesResponse.url,
     };
